783c03a3dd89e1a7a4a26033ee35515cfd278ed1
Andrew Shepherd <shepherd_a@hotmail.com>
2019-08-24 09:03:04 +1000

    Major speed up to WordDatabase

--- src/solve-crozzle/WordDatabase.cs
+++ src/solve-crozzle/WordDatabase.cs
@@ -8,36 +8,58 @@ namespace solve_crozzle
 {
     public class WordDatabase
 	{
-		private ImmutableHashSet<string> AvailableWords = ImmutableHashSet<string>.Empty;
-		private Dictionary<string, ImmutableHashSet<String>> WordLookup = new Dictionary<string, ImmutableHashSet<string>>();
+		private string[] _wordArray;
+		private Dictionary<string, int> _wordArrayIndex;
+		private bool[] _wordAvailability;
+		private Dictionary<string, List<int>> WordLookup = new Dictionary<string, List<int>>();
 
 		private WordDatabase()
 		{
 		}
 
-		internal WordDatabase Remove(string word) =>
-			new WordDatabase
+		internal WordDatabase Remove(string word)
+		{
+			var wordAvailability = new bool[_wordAvailability.Length];
+			_wordAvailability.CopyTo(wordAvailability, 0);
+			wordAvailability[_wordArrayIndex[word]] = false;
+			return new WordDatabase
 			{
-				AvailableWords = this.AvailableWords.Remove(word),
+				_wordArray = this._wordArray,
+				_wordArrayIndex = this._wordArrayIndex,
+				_wordAvailability = wordAvailability,
 				WordLookup = this.WordLookup
 			};
+		}
 
-		public override int GetHashCode() =>
-			HashUtils.GenerateHash(WordLookup) ^ WordLookup.GetHashCode();
+		public override int GetHashCode()
+		{
+			int hash = 0;
+			for(int i = 0; i < _wordAvailability.Length; ++i)
+			{
+				hash ^= HashUtils.RotateLeft(_wordAvailability[i] ? 1 : 0, i%32);
+			}
+			return hash;
+		}
 
 		public override bool Equals(object obj) =>
 			object.ReferenceEquals(this, obj)
 			|| (
 				(obj is WordDatabase wd)
-				&& this.AvailableWords.SetEquals(wd.AvailableWords)
+				&& Enumerable.SequenceEqual(this._wordAvailability, wd._wordAvailability)
 			);
 
 		public static WordDatabase Generate(IEnumerable<string> words)
 		{
+
 			var wordDatabase = new WordDatabase();
-			foreach (var word in words)
+			wordDatabase._wordArray = words.ToArray();
+			wordDatabase._wordArrayIndex = new Dictionary<string, int>();
+			wordDatabase._wordAvailability = new bool[wordDatabase._wordArray.Length];
+			for(int arrayIndex = 0; arrayIndex < wordDatabase._wordArray.Length; ++arrayIndex)	
 			{
-				wordDatabase.AvailableWords = wordDatabase.AvailableWords.Add(word);
+				string word = wordDatabase._wordArray[arrayIndex];
+				wordDatabase._wordArrayIndex[word] = arrayIndex;
+				wordDatabase._wordAvailability[arrayIndex] = true;
 				for (int i = 0; i < word.Length; ++i)
 				{
 					for (int j = 1; j + i <= word.Length; ++j)
@@ -45,14 +67,15 @@ namespace solve_crozzle
 						var substring = word.Substring(i, j);
 						if (wordDatabase.WordLookup.TryGetValue(substring, out var existingList))
 						{
-							if (!existingList.Contains(word))
+							if (!existingList.Contains(arrayIndex))
 							{
-								wordDatabase.WordLookup[substring] = existingList.Add(word);
+								existingList.Add(arrayIndex);
 							}
-
 						}
 						else
-							wordDatabase.WordLookup[word.Substring(i, j)] = ImmutableHashSet<string>.Empty.Add(word);
+						{
+							wordDatabase.WordLookup[word.Substring(i, j)] = new List<int>(new[] { arrayIndex });
+						}
 					}
 				}
 			}
@@ -61,10 +84,16 @@ namespace solve_crozzle
 
 		public IEnumerable<string> ListAvailableMatchingWords(string word)
 		{
-			if (this.WordLookup.TryGetValue(word, out var wordList))
-				return wordList.Intersect(this.AvailableWords);
-			else
-				return Enumerable.Empty<string>();
+			if (this.WordLookup.TryGetValue(word, out var wordIndexList))
+			{
+				foreach(var wordIndex in wordIndexList)
+				{
+					if(this._wordAvailability[wordIndex])
+					{
+						yield return this._wordArray[wordIndex];
+					}
+				}
+			}
 		}
 
 		public bool CanMatchWord(string word) =>
